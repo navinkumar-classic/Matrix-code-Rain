6d3ebea8964912588c66d74f464e1c8541628bc2
Navin_Kumar <121486045+navinkumar-classic@users.noreply.github.com>
2024-02-27 10:42:30 +0530

    Create matrix.css

---
+++ matrix.css
@@ -0,0 +1,76 @@
+body{
+    background-color: black;
+    overflow: hidden;
+}
+.head{
+    color: rgb(144,238,144,1);
+    line-height: 0%;
+    font-size: 5em;
+    font-weight:bold;
+    font-family: "Courier New";
+    word-spacing: 10px;
+    letter-spacing: 20px;
+    text-shadow: 0 0 3px rgb(144,238,144,0.5), 0 0 6px rgb(144,238,144,0.5);
+}
+#mainbox{
+    position: absolute;
+    top: 38%;
+    left: 26.5%;
+    height: 27%;
+    width: 47%;
+    background-color: transparent;
+}
+.jk{
+    position: absolute;
+    bottom: 100%;
+    width:10px;
+    color: rgb(144,238,144,1);
+    line-height: 85%;
+    font-size: x-large;
+    font-weight:bold;
+    font-family: "Courier New";
+    text-shadow: 0 0 4px rgb(144,238,144,1), 0 0 8px rgb(144,238,144,1);
+    animation-name:hmm;
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+}
+@keyframes hmm{
+    from{
+      bottom: 100%; 
+    }
+    to{
+      bottom: -100%;
+    }
+}
+.blink{
+    background-color:white;
+    color:transparent;
+    box-shadow: 0 0 3px white, 0 0 6px white;
+    animation-name:blink;
+    animation-duration: 0.15s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    animation-direction: alternate;
+    text-shadow: none;
+}
+@keyframes blink {
+    from{
+      background-color: white;
+      box-shadow: 0 0 3px white, 0 0 6px white;
+    }
+    to{
+      background-color: black;
+      box-shadow: none;
+    }
+}
+#searchbar{
+  position: absolute;
+  width: 100%;
+  background-color: #181818;
+  height: 50px;
+  border-style: none;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  bottom: 0%;
+}

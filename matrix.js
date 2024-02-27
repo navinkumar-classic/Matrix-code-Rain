e53ec243e5f077ca26c67a6f51c20ba0eaf7dd4c
Navin_Kumar <121486045+navinkumar-classic@users.noreply.github.com>
2024-02-27 10:47:58 +0530

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,76 +1,70 @@
-body{
-    background-color: black;
-    overflow: hidden;
-}
-.head{
-    color: rgb(144,238,144,1);
-    line-height: 0%;
-    font-size: 5em;
-    font-weight:bold;
-    font-family: "Courier New";
-    word-spacing: 10px;
-    letter-spacing: 20px;
-    text-shadow: 0 0 3px rgb(144,238,144,0.5), 0 0 6px rgb(144,238,144,0.5);
-}
-#mainbox{
-    position: absolute;
-    top: 38%;
-    left: 26.5%;
-    height: 27%;
-    width: 47%;
-    background-color: transparent;
-}
-.jk{
-    position: absolute;
-    bottom: 100%;
-    width:10px;
-    color: rgb(144,238,144,1);
-    line-height: 85%;
-    font-size: x-large;
-    font-weight:bold;
-    font-family: "Courier New";
-    text-shadow: 0 0 4px rgb(144,238,144,1), 0 0 8px rgb(144,238,144,1);
-    animation-name:hmm;
-    animation-duration: 5s;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
-}
-@keyframes hmm{
-    from{
-      bottom: 100%; 
+
+
+let arr = ["101100101101100101001010","10100011100101000001000010010010001111","1111001010010100010","0100101010101001010010101010101","111010101010010"];
+let t = [5,8,3,3.5,10,5,4,6,4,2,5,3,4,6,4,5,8,3,3.5,10,5,4,6,4,2,5,3,4,6,4];
+
+
+function spawn(a,l,s,d){
+  const para = document.createElement("p");
+  para.className = "jk";
+  para.style.left = l + "px";
+  para.style.animationDuration = s + "s";
+  para.style.animationDelay = d + "s";
+  
+  for(let i = 0; i < a.length; i++){
+    const temp = document.createElement("span");
+    const node = document.createTextNode(a[i] + " ");
+    temp.style.color = "rgb(144,238,144,"+ (i*1)/a.length +")"
+    temp.style.textShadow = "0 0 3px rgb(144,238,144,"+ (i*1)/a.length +"), 0 0 6px rgb(144,238,144,"+(i*1)/a.length +")";
+    temp.appendChild(node);
+    
+    function glitch(){
+      let chan = Math.round(Math.random());
+      if(chan == 1){
+        id = setInterval(frame, 150);
+        let pos = 0;
+        function frame() {
+           let a = temp.innerHTML;
+           if(a == "1 "){
+             temp.innerHTML = "0" + " ";
+            }
+           else{
+             temp.innerHTML = "1" + " ";
+           }
+        }
+      }  
     }
-    to{
-      bottom: -100%;
+    
+    let j = setInterval(glitch, 2000);
+    
+    
+    para.appendChild(temp);
+  }
+  
+  const temp = document.createElement("span");temp.className = "blink";
+  const node = document.createTextNode(".");
+  temp.appendChild(node);
+  
+  para.appendChild(temp);
+  const element = document.getElementById("container");
+  element.appendChild(para);
+}
+
+for(let i = 0; i < 30; i++){
+    if( i < (30/2)){
+        let ai = Math.floor(Math.random() * 5);
+        let speed = Math.random() * 9 + 3;
+        let delay = Math.random() * 4 + 2;
+        spawn(arr[ai],i*25,t[i],delay); // 3 - 9 // 2 - 4 
     }
-}
-.blink{
-    background-color:white;
-    color:transparent;
-    box-shadow: 0 0 3px white, 0 0 6px white;
-    animation-name:blink;
-    animation-duration: 0.15s;
-    animation-iteration-count: infinite;
-    animation-timing-function: linear;
-    animation-direction: alternate;
-    text-shadow: none;
-}
-@keyframes blink {
-    from{
-      background-color: white;
-      box-shadow: 0 0 3px white, 0 0 6px white;
+    else{
+        let ai = Math.floor(Math.random() * 5);
+        let speed = Math.random() * 9 + 3;
+        let delay = Math.random() * 4 + 2;
+        spawn(arr[ai],i*25 + 780,t[i],delay); // 3 - 9 // 2 - 4 
     }
-    to{
-      background-color: black;
-      box-shadow: none;
-    }
+    
 }
-#searchbar{
-  position: absolute;
-  width: 100%;
-  background-color: #181818;
-  height: 50px;
-  border-style: none;
-  border: 1px solid #ccc;
-  border-radius: 10px;
-  bottom: 0%;
-}
+
+
+
